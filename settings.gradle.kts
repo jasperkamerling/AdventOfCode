7113b7996a4b60cb605001d8e7618afb4fdb26fd
Jasper Kamerling <jasper.kamerling@hotmail.com>
2024-11-07 23:16:45 +0100

    Simplify Gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,10 +9,7 @@ include(
     "Advent2015",
     "Advent2020",
     "Advent2021",
-    "Advent2022",
+    "Advent2022:day01",
     "Advent2023",
+    "Advent2024:day01",
 )
-
-(1..30).map { it.toString() }
-    .map { it.padStart(2, '0') }
-    .forEach { include("Advent2024:day${it}") }
