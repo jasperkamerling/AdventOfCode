313fe9cb37b7a34e88b98fe337f133d11266a70d
Jasper Kamerling <jasper.kamerling@hotmail.com>
2024-12-01 10:12:36 +0100

    2024 Day 1 part 2

--- Advent2024/day01/src/Day01.kt
+++ Advent2024/day01/src/Day01.kt
@@ -3,20 +3,23 @@ import kotlin.math.absoluteValue
 class Day01(fileName: String) {
     private val input = fileFromResources(fileName).readLines()
         .filterNot { it.isEmpty() }
+        .map { it.split("   ") }
+        .map { Pair(it[0].toInt(), it[1].toInt()) }.fold(Pair<List<Int>, List<Int>>(listOf(), listOf()))
+        { acc, pair -> Pair(acc.first + pair.first, acc.second + pair.second) }
 
     fun part1(): Int =
-         input.map { it.split("   ") }
-            .map { Pair(it[0].toInt(), it[1].toInt()) }
-            .fold(Pair<MutableList<Int>, MutableList<Int>>(mutableListOf(), mutableListOf()))
-            { acc, pair -> Pair((acc.first + pair.first).toMutableList(), (acc.second + pair.second).toMutableList()) }
-            .let { Pair(it.first.sorted(), it.second.sorted()) }
-            .let { lists -> lists.first.mapIndexed { index, leftLocation -> leftLocation - lists.second[index]  }
-                .sumOf { it.absoluteValue } }
+         input.let { Pair(it.first.sorted(), it.second.sorted()) }
+            .let { lists ->
+                lists.first.mapIndexed { index, leftLocation -> leftLocation - lists.second[index] }
+                    .sumOf { it.absoluteValue }
+            }
 
 
-    fun part2(): Int {
-        return input.hashCode()
-    }
+    fun part2(): Int =
+         input.let { pair -> pair.first.map {
+                    leftNumber -> pair.second.count { it == leftNumber } * leftNumber
+                }
+            }.sum()
 }
 
 fun main() {
@@ -27,6 +30,6 @@ fun main() {
     check(day01Test.part1() == 11)
     println(day01.part1())
 
-    check(day01Test.part2() == 1)
+    check(day01Test.part2() == 31)
     println(day01.part2())
 }
