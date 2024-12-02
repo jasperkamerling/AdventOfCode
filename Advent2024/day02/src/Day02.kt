b89c1bb8f1065a649e1de8c8d7574d34cbd82cec
Jasper Kamerling <jasper.kamerling@hotmail.com>
2024-12-02 08:30:02 +0100

    2024 Day 02 Part 2

--- Advent2024/day02/src/Day02.kt
+++ Advent2024/day02/src/Day02.kt
@@ -4,14 +4,17 @@ data class Level(val previous: Int?, val increasing: Boolean = true, val decreas
 
 class Day02(fileName: String) {
     private val input = fileFromResources(fileName).readLines()
+        .filterNot { it.isEmpty() }
         .map { line -> line.split(" ").map {it.toInt()} }
 
     fun part1(): Int {
-        return input.map {
-                it.fold(Level(null))
-                    {acc, i -> checkLevel(acc, i) }
-            }
-            .count { it.safe }
+        return input.count { isRowSafe(it) }
+    }
+
+    private fun isRowSafe(row: List<Int>): Boolean  {
+        return row.fold(Level(null))
+            {acc, i -> checkLevel(acc, i) }
+            .safe
     }
 
     private fun checkLevel(level: Level, next: Int): Level {
@@ -31,7 +34,15 @@ class Day02(fileName: String) {
     }
 
     fun part2(): Int {
-        return input.hashCode()
+        return input
+            .count { checkAnySafeLevels(it) }
+    }
+
+    fun checkAnySafeLevels(row: List<Int>): Boolean {
+        if(isRowSafe(row)) return true
+        return row.mapIndexed { index, number ->
+          row.toMutableList().apply{removeAt(index)}.toList()
+        }.any { isRowSafe(it) }
     }
 
 }
@@ -44,6 +55,6 @@ fun main() {
     check(day02Test.part1() == 2)
     println(day02.part1())
 
-    check(day02Test.part2() == 1)
+    check(day02Test.part2() == 4)
     println(day02.part2())
 }
