4a5f19ef03ffd6766e52a2b8b8d2c9fb84754e2a
Jasper Kamerling <jasper.kamerling@hotmail.com>
2024-12-03 08:19:29 +0100

    2024 Day 03 Part 2

--- Advent2024/day03/src/Day03.kt
+++ Advent2024/day03/src/Day03.kt
@@ -3,17 +3,35 @@ import kotlin.text.Regex
 class Day03(fileName: String) {
     private val input = fileFromResources(fileName).readText()
 
-    private val pattern = Regex("mul\\((\\d+),(\\d+)\\)")
+    private val partOnePattern = Regex("mul\\((\\d+),(\\d+)\\)")
+    private val partTwoPattern = Regex("(mul\\((\\d+),(\\d+)\\))|(do\\(\\))|(don't\\(\\))")
 
     fun part1(): Int {
-        return pattern.findAll(input)
+        return partOnePattern.findAll(input)
             .map {it.groupValues }
             .map {Pair(it[1].toInt(), it[2].toInt())}
             .sumOf {it.first * it.second}
     }
 
+    data class Result(val sum: Int = 0, val enabled: Boolean = true)
+
     fun part2(): Int {
-        return input.hashCode()
+        return partTwoPattern.findAll(input)
+            .map {it.groupValues.drop(1).filterNot {it.isEmpty()} }
+            .fold(Result()) { r, i -> handleInstruction(r, i)}
+            .sum
+    }
+
+    fun handleInstruction(result: Result, instruction: List<String>) : Result{
+        return if(result.enabled && instruction[0].startsWith("mul")) {
+            result.copy(sum = result.sum + instruction[1].toInt() * instruction[2].toInt())
+        } else if (instruction.contains("do()")) {
+            result.copy(enabled = true)
+        } else if (instruction.contains("don't()")) {
+            result.copy(enabled = false)
+        } else {
+            result
+        }
     }
 }
 
@@ -25,6 +43,7 @@ fun main() {
     check(day03Test.part1() == 161)
     println(day03.part1())
 
-    check(day03Test.part2() == 1)
+    val day03Test2 = Day03("test03_2.txt")
+    check(day03Test2.part2() == 48)
     println(day03.part2())
 }
