6b8b572e797abfa2c638d8b52f7fc2d043bdcb04
Jasper Kamerling <jasper.kamerling@hotmail.com>
2024-12-06 09:43:16 +0100

    2024 Day 06 Part 2

--- Advent2024/day06/src/Day06.kt
+++ Advent2024/day06/src/Day06.kt
@@ -1,22 +1,46 @@
 import Direction.*
+import LocationType.*
 
-class Location(val isCrate: Boolean, var isVisited: Boolean = false)
+typealias Map = List<List<Location>>
+
+enum class LocationType {
+    EMPTY, CRATE, OBSTACLE;
+}
+
+class Location(var type: LocationType, var isVisited: Boolean = false) {
+    fun isObstacle(): Boolean {
+        return type == CRATE || type == OBSTACLE
+    }
+
+    fun display(): Char {
+        return when {
+            isVisited -> 'X'
+            type == EMPTY -> '.'
+            type == CRATE -> '#'
+            type == OBSTACLE -> 'O'
+            else -> ' '
+        }
+    }
+}
+
 enum class Direction { UP, DOWN, LEFT, RIGHT }
 
-data class Guard(var x: Int, var y: Int, var direction: Direction, val map: List<List<Location>>) {
-
+data class Guard(var x: Int, var y: Int, var direction: Direction, val map: Map, var steps: Int = 0) {
     fun visitLocation() {
         map[x][y].isVisited = true
     }
 
+    fun isFacingObstacle(): Boolean {
+        return getFacingLocation()?.isObstacle() == true
+    }
 
-    fun isFacingCrate(): Boolean {
+    private fun getFacingLocation(): Location? {
         return when (direction) {
             UP -> map.getOrNull(x - 1)?.getOrNull(y)
             RIGHT -> map.getOrNull(x)?.getOrNull(y + 1)
             DOWN -> map.getOrNull(x + 1)?.getOrNull(y)
             LEFT -> map.getOrNull(x)?.getOrNull(y - 1)
-        }?.isCrate == true
+        }
     }
 
     fun changeDirection() {
@@ -29,7 +53,8 @@ data class Guard(var x: Int, var y: Int, var direction: Direction, val map: List
     }
 
     fun move() {
-        return when (direction) {
+        steps++
+        when (direction) {
             UP -> x -= 1
             RIGHT -> y += 1
             DOWN -> x += 1
@@ -40,6 +65,7 @@ data class Guard(var x: Int, var y: Int, var direction: Direction, val map: List
     fun inBounds(): Boolean {
         return map.getOrNull(x)?.getOrNull(y) != null
     }
+
 }
 
 class Day06(fileName: String) {
@@ -47,13 +73,19 @@ class Day06(fileName: String) {
         .map { it.toCharArray() }
 
 
-    private fun getMap(): List<List<Location>> = input.map { it.map { char -> Location(char == '#') } }
+    private fun getMap(): Map = input.map {
+        it.map { char -> Location(determineLocationType(char)) }
+    }
 
-    private fun getGuard(): Guard {
+    private fun determineLocationType(char: Char): LocationType {
+        return if (char == '#') CRATE else EMPTY
+    }
+
+    private fun getGuard(map: Map): Guard {
         input.forEachIndexed { index, chars ->
             chars.forEachIndexed { charIndex, char ->
                 if (char == '^') {
-                    return@getGuard Guard(index, charIndex, UP, getMap())
+                    return@getGuard Guard(index, charIndex, UP, map)
                 }
             }
         }
@@ -61,7 +93,7 @@ class Day06(fileName: String) {
     }
 
     fun part1(): Int {
-        return getGuard()
+        return getGuard(getMap())
             .also { walkMap(it) }
             .map.sumOf { row -> row.count { it.isVisited } }
     }
@@ -69,7 +101,7 @@ class Day06(fileName: String) {
     private fun walkMap(guard: Guard): Guard {
         do {
             guard.visitLocation()
-            if (guard.isFacingCrate()) {
+            if (guard.isFacingObstacle()) {
                 guard.changeDirection()
             }
 
@@ -80,14 +112,41 @@ class Day06(fileName: String) {
     }
 
     fun part2(): Int {
-        return input.hashCode()
+        val maps: List<Map> = getMap().flatMapIndexed { xIndex, x ->
+            x.filterNot { it.type == CRATE }
+                .mapIndexed { yIndex, _ ->
+                    getMap().also { it[xIndex][yIndex].apply { type = OBSTACLE } }
+                }
+        }
+
+        return maps.count { isInfinite(getGuard(it)) }
+    }
+
+    private fun isInfinite(guard: Guard): Boolean {
+        do {
+            if (guard.steps > 10000) {
+                return true
+            }
+            if (guard.isFacingObstacle()) {
+                guard.changeDirection()
+            }
+            guard.move()
+        } while (guard.inBounds())
+
+        return false
+    }
+
+    private fun Map.print() {
+        println()
+        this.forEach {
+            println(it.map { it.display() }.joinToString(""))
+        }
     }
 }
 
 fun main() {
     val day06Test = Day06("test06.txt")
     val day06 = Day06("real06.txt")
-
 
     check(day06Test.part1() == 41)
     println("Test part 1 complete")
