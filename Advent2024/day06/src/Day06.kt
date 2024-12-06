1b2c373b395b61a1a59476b6d87fb2f64db3b6c2
Jasper Kamerling <jasper.kamerling@hotmail.com>
2024-12-06 08:34:13 +0100

    2024 Day 06 Part 1

--- Advent2024/day06/src/Day06.kt
+++ Advent2024/day06/src/Day06.kt
@@ -2,52 +2,11 @@ import Direction.*
 
 class Location(val isCrate: Boolean, var isVisited: Boolean = false)
 enum class Direction { UP, DOWN, LEFT, RIGHT }
-data class Position(val x: Int, val y: Int, val direction: Direction)
-class Day06(fileName: String) {
-    private val input = fileFromResources(fileName).readLines()
-        .map { it.toCharArray() }
 
-    private val map: List<List<Location>> = input.map { it.map { char -> Location(char == '#') } }
-    private val startingLocation = determineStartingPosition()
+data class Guard(var x: Int, var y: Int, var direction: Direction) {
 
-    private fun determineStartingPosition(): Position {
-        input.forEachIndexed { index, chars ->
-            chars.forEachIndexed { charIndex, char ->
-                if (char == '^') {
-                    return@determineStartingPosition Position(index, charIndex, UP)
-                }
-            }
-        }
-        throw Exception("Did not find starting location")
-    }
-
-    fun part1(): Int {
-        var position = startingLocation
-        do {
-            val facingLocation = getFacingLocation(position) ?: continue
-
-            if (facingLocation.isCrate) {
-            }
-        } while (inBounds(position))
-
-        return map.sumOf { row -> row.count { it.isVisited } }
-    }
-
-    private fun getCurrentLocation(position: Position): Location {
-        return map[position.x][position.y]
-    }
-
-    private fun getFacingLocation(position: Position): Location? {
-        return when (position.direction) {
-            UP -> map.getOrNull(position.x - 1)?.getOrNull(position.y)
-            RIGHT -> map.getOrNull(position.x)?.getOrNull(position.y + 1)
-            DOWN -> map.getOrNull(position.x - 1)?.getOrNull(position.y)
-            LEFT -> map.getOrNull(position.x)?.getOrNull(position.y - 1)
-        }
-    }
-
-    fun changeDirection(direction: Direction): Direction {
-        return when (direction) {
+    fun changeDirection() {
+        direction = when (direction) {
             UP -> RIGHT
             RIGHT -> DOWN
             DOWN -> LEFT
@@ -55,13 +14,66 @@ class Day06(fileName: String) {
         }
     }
 
-    fun move(position: Position): Position {
-        position.
+    fun move() {
+        return when (direction) {
+            UP -> x -= 1
+            RIGHT -> y += 1
+            DOWN -> x += 1
+            LEFT -> y -= 1
+        }
+    }
+}
+
+class Day06(fileName: String) {
+    private val input = fileFromResources(fileName).readLines()
+        .map { it.toCharArray() }
+
+    private val map: List<List<Location>> = input.map { it.map { char -> Location(char == '#') } }
+    private val startingLocation = determineStartingPosition()
+
+    private fun determineStartingPosition(): Guard {
+        input.forEachIndexed { index, chars ->
+            chars.forEachIndexed { charIndex, char ->
+                if (char == '^') {
+                    return@determineStartingPosition Guard(index, charIndex, UP)
+                }
+            }
+        }
+        throw Exception("Did not find starting location")
     }
 
-    fun inBounds(position: Position): Boolean {
-        return position.x > input.size || position.y > input.first().size
+    fun part1(): Int {
+        val guard = startingLocation
+        do {
+            getCurrentLocation(guard).isVisited = true
+            val facingLocation = getFacingLocation(guard) ?: break
+
+            if (facingLocation.isCrate) {
+                guard.changeDirection()
+            }
+
+            guard.move()
+        } while (true)
+
+        return map.sumOf { row -> row.count { it.isVisited } }
     }
+
+    private fun getCurrentLocation(guard: Guard): Location {
+        return map[guard.x][guard.y]
+    }
+
+    private fun getFacingLocation(guard: Guard): Location? {
+        return when (guard.direction) {
+            UP -> map.getOrNull(guard.x - 1)?.getOrNull(guard.y)
+            RIGHT -> map.getOrNull(guard.x)?.getOrNull(guard.y + 1)
+            DOWN -> map.getOrNull(guard.x + 1)?.getOrNull(guard.y)
+            LEFT -> map.getOrNull(guard.x)?.getOrNull(guard.y - 1)
+        }
+    }
+
+//    fun inBounds(position: Position): Boolean {
+//        return position.x > input.size || position.y > input.first().size
+//    }
 
     fun part2(): Int {
         return input.hashCode()
@@ -73,7 +85,8 @@ fun main() {
     val day06 = Day06("real06.txt")
 
 
-    check(day06Test.part1() == 0)
+    check(day06Test.part1() == 41)
+    println("Test part 1 complete")
     println(day06.part1())
 
     check(day06Test.part2() == 1)
