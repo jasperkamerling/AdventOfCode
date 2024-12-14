9477a910d64e42e4c919e43c015564204b1f26a2
Jasper Kamerling <jasper.kamerling@alliander.com>
2024-12-14 11:11:57 +0100

    2024 Day 08 improvements

--- Advent2024/day08/src/Day08.kt
+++ Advent2024/day08/src/Day08.kt
@@ -1,30 +1,33 @@
+import kotlin.time.measureTimedValue
+
 data class Location(val char: Char, val cords: Cords)
 
 class Day08(fileName: String) {
     private val input = fileFromResources(fileName).readLines()
-    private val locations = input
-        .flatMapIndexed { x, row ->
-            row.mapIndexed { y, char -> Location(char, x toc y) }
-        }
-        .filterNot { it.char == '.' || it.char == '#'}
+    private val locations =
+        input
+            .flatMapIndexed { x, row ->
+                row.mapIndexed { y, char -> Location(char, x toc y) }
+            }
+            .filterNot { it.char == '.' || it.char == '#' }
 
-    private val locationsPerChar = locations.groupBy { it.char }
-        .mapValues { it.value.map { it.cords } }
+    private val locationsPerChar =
+        locations
+            .groupBy { it.char }
+            .mapValues { it.value.map { it.cords } }
 
-    fun part1(): Int {
-        return locations
-            .flatMap { determineDiff(it.cords, locationsPerChar[it.char]!!) }
+    fun part1(): Int =
+        locations
+            .map { it.cords to determineDiffs(it.cords, locationsPerChar[it.char]!!) }
+            .flatMap { (source, diffs) -> diffs.flatMap { listOf(it + source, it - source) } }
             .distinctBy { it }
             .filter { it.x in input.indices && it.y in input.first().indices }
             .size
-    }
 
-    private fun determineDiff(cords: Cords, otherCords: List<Cords>): List<Cords> {
-        return otherCords
+    private fun determineDiffs(cords: Cords, otherCords: List<Cords>): List<Cords> =
+        otherCords
             .filterNot { cords.x == it.x && cords.y == it.y }
             .map { cords.diff(it) }
-            .flatMap { listOf(it + cords, it - cords ) }
-    }
 
     fun part2(): Int {
         return locations.hashCode()
@@ -36,8 +39,8 @@ fun main() {
     val input = Day08("real08.txt")
 
     check(testInput.part1() == 14)
-    println(input.part1())
+    measureTimedValue { input.part1() }.let { println("${it.value} - ${it.duration}") }
 
     check(testInput.part2() == 1)
-    println(input.part2())
+    measureTimedValue { input.part2() }.let { println("${it.value} - ${it.duration}") }
 }
