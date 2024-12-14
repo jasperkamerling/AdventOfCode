bef22c02976a7685265a3f2e1e586d1b1b29d835
Jasper Kamerling <jasper.kamerling@alliander.com>
2024-12-14 12:06:34 +0100

    2024 Day 08 part 2

--- Advent2024/day08/src/Day08.kt
+++ Advent2024/day08/src/Day08.kt
@@ -22,6 +22,7 @@ class Day08(fileName: String) {
             .flatMap { (source, diffs) -> diffs.flatMap { listOf(it + source, it - source) } }
             .distinctBy { it }
             .filter { it.x in input.indices && it.y in input.first().indices }
+            .also { (locations + it.map { Location('#', it) }).printMap() }
             .size
 
     private fun determineDiffs(cords: Cords, otherCords: List<Cords>): List<Cords> =
@@ -29,9 +30,19 @@ class Day08(fileName: String) {
             .filterNot { cords.x == it.x && cords.y == it.y }
             .map { cords.diff(it) }
 
-    fun part2(): Int {
-        return locations.hashCode()
-    }
+    // TODO figure out what i'm doing wrong
+    fun part2(): Int =
+        locations
+            .map { it.cords to determineDiffs(it.cords, locationsPerChar[it.char]!!) }
+            .map { (source, diffs) -> source to multiplyDiffs(diffs) }
+            .flatMap { (source, diffs) -> diffs.flatMap { listOf(source + it, source - it) } }
+            .distinctBy { it }
+            .filter { it.x in input.indices && it.y in input.first().indices }
+            .also { (locations + it.map { Location('#', it) }).printMap() }
+            .size
+
+    fun multiplyDiffs(diffs: List<Cords>) =
+        diffs.flatMap { cords -> (0..50).map { i -> cords * i } }
 }
 
 fun main() {
@@ -41,6 +52,9 @@ fun main() {
     check(testInput.part1() == 14)
     measureTimedValue { input.part1() }.let { println("${it.value} - ${it.duration}") }
 
-    check(testInput.part2() == 1)
+    check(Day08("test08-2.txt").part2() == 9)
+    check(Day08("test08-3.txt").part2() == 13)
+    check(Day08("test08-4.txt").part2() == 4)
+    check(testInput.part2() == 34)
     measureTimedValue { input.part2() }.let { println("${it.value} - ${it.duration}") }
 }
--- Utilities/src/Cords.kt
+++ Utilities/src/Cords.kt
@@ -1,10 +1,45 @@
+data class Location(val char: Char, val cords: Cords)
+
 data class Cords(val x: Int, val y: Int) {
     operator fun plus(other: Cords) =
         this.x + other.x toc this.y + other.y
+
     operator fun minus(other: Cords) =
         this.x - other.x toc this.y - other.y
+
+    operator fun times(other: Int) =
+        this.x * other toc this.y * other
+
     fun diff(other: Cords): Cords {
         return this.x - other.x toc this.y - other.y
+    }
+}
+
+fun List<Cords>.printMap(hit: Char = '#', empty: Char = '.') {
+    val maxX = this.maxOf { it.x }
+    val maxY = this.maxOf { it.y }
+    println()
+    (0..maxX).forEach { x ->
+        (0..maxY).forEach { y ->
+            if(this.any { it.x == x && it.y == y }) {
+                print(hit)
+            } else {
+                print(empty)
+            }
+        }
+        println()
+    }
+}
+
+fun List<Location>.printMap(empty: Char = '.') {
+    val maxX = this.maxOf { it.cords.x }
+    val maxY = this.maxOf { it.cords.y }
+    println()
+    (0..maxX).forEach { x ->
+        (0..maxY).forEach { y ->
+            print(this.find { it.cords.x == x && it.cords.y == y }?.char ?: empty)
+        }
+        println()
     }
 }
 
